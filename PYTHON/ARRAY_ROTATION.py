c2317eda3b8294eebe8033abf05d1cf3c379afc7
abhi-03 <53304621+abhi-03@users.noreply.github.com>
2019-07-27 15:53:53 +0530

    Update ARRAY_ROTATION.py
    WITH USER INPUT

--- PYTHON/ARRAY_ROTATION.py
+++ PYTHON/ARRAY_ROTATION.py
@@ -1,24 +1,29 @@
-def leftRotate(arr, d, n):
-    for i in range(d):
-        leftRotatebyOne(arr, n)
+from array import *
+
+def rotation(a,n):
+    temp=a[0]
+    for i in range(n-1):
+        a[i]=a[i+1]
+
+    a[n-1]=temp
+
+def  rotate(a,d,n):
+     for i in range (d):
+         rotation(a,n)
+
+def  disp(a,n):
+     for i in range(n):
+         print("% d" % a[i], end=" ")
+
+a=array('i',[])
+
+f=int(input("ENTER THE LENGTH OF ARRAY"))
+
+for i in range(f):
+     x=int(input("ENTER THE NEXT VALUE"))
+     a.append(x)
+d=int(input("ENTER VALUE FOR D"))
+rotate(a,d,f)
+disp(a,f)
 
 
-
-
-def leftRotatebyOne(arr, n):
-    temp = arr[0]
-    for i in range(n - 1):
-        arr[i] = arr[i + 1]
-    arr[n - 1] = temp
-
-
-def printArray(arr, size):
-    for i in range(size):
-        print("% d" % arr[i], end=" ")
-
-/
-
-
-arr = [1, 2, 3, 4, 5, 6, 7]
-leftRotate(arr, 2, 7)
-printArray(arr, 7)
